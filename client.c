c4e0a24f50cfee47df29ff228822548cb1cd3320
koellemichael <koelle.michael@gmail.com>
2016-11-21 21:11:32 +0100

    Update client.c

--- client.c
+++ client.c
@@ -10,16 +10,15 @@ int main (int argc, char **argv){
 
   char gameid [12];
   if(argc<2){                         //Test: Wird eine Game-ID übergeben
-    perror("no game id");             //Keine Game-ID vorhanden
+    perror("No game id");             //Keine Game-ID vorhanden
   }
   else{                               //Game-ID vorhanden
 
     if (strlen(argv[1])!=13){         //Test: Ist die Game-ID 13-stellig
-      perror("invalid game id");      //Game-ID zu kurz oder zu lang
+      perror("Invalid game id");      //Game-ID zu kurz oder zu lang
     }
     else {                            //13-stellige Game-ID übergeben
-      for(int i=0; i<13; i++)         //for-Schleife um Game-ID auszulesen
-      {
+      for(int i=0; i<13; i++){         //for-Schleife um Game-ID auszulesen
         gameid[i]=argv[1][i];         //speichern der Game-ID in gameid
       }
       printf("%s\n", gameid);
