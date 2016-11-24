32c61f11449dfa46cbe63014f6968e12d990c928
KathiWinter <kathi.winter@t-online.de>
2016-11-24 11:23:14 +0100

    Übergabe fd an performConnection
    Stimmt das so?

--- verbindungGameserver.c
+++ verbindungGameserver.c
@@ -8,7 +8,6 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 
 /**
@@ -17,10 +16,12 @@
  */
 
 
-
+//main wird noch umbenannt, wenn es mit den anderen Dateien verbunden wird. Nur zum Test.
 int main(){
     //Variablen deklarieren
     
+    //Diese hier wird noch geändert zu Lauras Version
+    const char hostname[] = "sysprak.priv.lab.nm.ifi.lmu.de";
     int sock;
     char ip[100];
     struct hostent *he;
@@ -31,16 +32,17 @@ int main(){
      * SOCK_STREAM: Typ des Sockets.(Gegensatz: SOCK_DGRAM). Stream Zuverlässiger.
      * 0: Default Transportprotokoll des Sockettyps. Von SOCK_STREAM ist dies TCP.
      */
-    
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1){
         perror("Konnte Socket nicht erzeugen\n");
     }
     
     
-    if( (he = gethostbyname(HOSTNAME)) == NULL){
+    //HOSTNAME in IP Adresse umwandeln
+    if( (he = gethostbyname(hostname)) == NULL){
         exit(1);
     }
+    
     
     struct sockaddr_in server;
     server.sin_family = PF_INET;
@@ -52,7 +54,12 @@ int main(){
     if(connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
         perror("connect fehlgeschlagen");
         return 1; 
+    } else {
+        printf("Mit dem Server erfolgreich verbunden.");
     }
+    
+    //Übergabe Filedeskriptor an Funktion performConnection()
+    performConnection(sock);
     
     close(sock);
     return 0;
