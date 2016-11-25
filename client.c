23b84009cd66ebf1e38003f2e0ff9c08adf90212
Laura <Laura.Haller@T-online.de>
2016-11-25 10:27:06 +0100

    Strcpy verwendet
    for Schleife raus, strcpy stattdessen

--- client.c
+++ client.c
@@ -18,11 +18,10 @@ int main (int argc, char **argv){
       perror("Invalid game id");      //Game-ID zu kurz oder zu lang
     }
     else {                            //13-stellige Game-ID übergeben
-      for(int i=0; i<13; i++){         //for-Schleife um Game-ID auszulesen
-        gameid[i]=argv[1][i];         //speichern der Game-ID in gameid
+        strcpy(gameid, argv[1]);           //kopieren des Strings nach gameid
       }
       printf("%s\n", gameid);
     }
-  }
+
 
 }
