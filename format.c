36c82160952306a54a69450ecae1c52cd9c60958
koellemichael <Michi@dhcp-10-176-80-127.dynamic.eduroam.mwn.de>
2016-12-09 10:09:03 +0100

    Serverinformationen in Variablen gespeichert

--- format.c
+++ format.c
@@ -2,6 +2,14 @@
 
 int pro = 1;                                                                                        //Prologvariable
 
+int version;
+char *gamekind;
+char *gamename;
+
+char *player;
+int playernumber;
+int totalplayers;
+
 /*
  *Funktion format formatiert die Nachrichten vom Server.
  *@param *input Nachricht vom Servers
@@ -12,68 +20,60 @@ char *format(char *input){
   char *out = malloc(256*sizeof(char));
 
     if(pro==1 && match(input, "MNM Gameserver .+accepting connections")){                          // Stimmen Prologvariable und Text überein
-      strcpy(out, "The MNM Gameserver ");                                                          // Ausgabe The MNM Gameserver <Versionsnummer> accepted the connection
-      strcat(out, substring(input, 14,strlen(input)-21));                                          // Versionsnummer
-      strcat(out, "accepted the connection.");
+      strcpy(out, "The MNM Gameserver v");                                                         // Ausgabe The MNM Gameserver <Versionsnummer> accepted the connection
+      char *sversion = substring(input, 16,strlen(input)-22);
+      version = atoi(sversion);                                                                    //Versionsnummer in globaler Variable speichern
+      strcat(out, sversion);                                                                      // Versionsnummer
+      strcat(out, " accepted the connection.");
       pro++;                                                                                       // Prolog erhöhen
-
-
     }else if(pro==2 && match(input, "Client version accepted - please send Game-ID to join")){     // Stimmen Prologvariable und Text überein
       strcpy(out,"The client version was accepted. Now please send Game-ID to join the game.");    // Ausgabe The client version was accepted. Now please send Game-ID to join the game
       pro++;                                                                                       // Prolog erhöhen
-
-
-
     }else if(pro==3 && match(input, "PLAYING .+")){                                                // Stimmen Prologvariable und Text überein
       strcpy(out, "We are playing the game ");                                                     // Ausgabe We are playing the game <Gamekind>
-      strcat(out, substring(input, 8,strlen(input)));                                              // Gamekind
+      gamekind = substring(input, 8,strlen(input));
+      strcat(out, gamekind);                                                                       // Gamekind
       pro++;                                                                                       // Prolog erhöhen
-
-
-
     }else if(pro==4 && match(input, ".+")){                                                        // Stimmen Prologvariable und Text überein
-      strcpy(out, "The games name is ");                                                          // Ausgabe The game's name is <Gamename>
+      strcpy(out, "The games name is ");                                                           // Ausgabe The game's name is <Gamename>
+      gamename = input;
       strcat(out, input);                                                                          // Gamename
       pro++;                                                                                       // Prolog erhöhen
-
-
     }else if(pro==5 && match(input, "YOU .+ .+")){                                                 // Stimmen Prologvariable und Text überein
-      strcpy(out, substring(input, 6,strlen(input)));                                              // Player name
+      player = substring(input, 6,strlen(input));
+      strcpy(out, player);                                                                         // Player name
       strcat(out, " you are player number ");                                                      // Ausgabe <Player name> you are player number <Player number>
-      strcat(out, substring(input, 4, 5));                                                         // Player number
+      char *splayernumber = substring(input, 4, 5);
+      playernumber = atoi(splayernumber);
+      strcat(out, splayernumber);                                                                   // Player number
       pro++;                                                                                       // Prolog erhöhen
-
-
     }else if(pro==6 && match(input, "TOTAL .+")){                                                  // Stimmen Prologvariable und Text überein
-      strcpy(out, substring(input,6,strlen(input)));                                               // Ausgabe <Total> player will take part in this game
-      strcat(out, " player will take part in this game.");                                         // Total
+      char *stotalplayers = substring(input,6,strlen(input));
+      totalplayers = atoi(stotalplayers);
+      strcpy(out, stotalplayers);                                                                  // Ausgabe <Total> player will take part in this game
+      if(totalplayers <2){
+        strcat(out, " player will take part in this game.");
+      }else{
+        strcat(out, " players will take part in this game.");
+      }
       pro++;                                                                                       // Prolog erhöhen
-
-
     }else if(match(input, "ENDPLAYERS")){                                                          // Input ENDPLAYERS
-      strcpy(out, "ENDPLAYERS - The prolog is finished!");                                         // Ausgabe ENDPLAYERS - The prolog is finished!
-
-
+      strcpy(out, "End of Prolog");                                                                // Ausgabe ENDPLAYERS - The prolog is finished!
     }else if(pro>=7 && match(input, ".+ .+ .+")){                                                  // Stimmen Prologvariable und Text überein
       strcpy(out, substring(input, 2,strlen(input)-2));                                            // Ausgabe <Player> (<Playernumber>) is ready/ not ready
       strcat(out," (");
       strcat(out, substring(input, 0,1));                                                          // (Playernumber)
       strcat(out,") is ");
-
-
       if(atoi(substring(input, strlen(input)-2, strlen(input)-1))==1){                              //Spieler bereit -> letzte Zahl = 1
         strcat(out, "ready");
       }
       else if(atoi(substring(input, strlen(input)-2, strlen(input)-1))==0){                         //Spieler nicht bereit -> letzte Zahl = 0
         strcat(out, "not ready");
       }
-
       pro++;                                                                                       // Prolog erhöhen
     }else{                                                                                         // Unbekannte Eingabe
       strcpy(out,"Unknown request");
     }
-
-
 
     if(input!=NULL){
       strcat(out,"\n");                                                                           // Anfügen Zeilenumbruch
