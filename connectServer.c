0f3cf9c84670b27f48a44c611685d504b3c2cd81
koellemichael <Michi@dhcp-10-180-27-101.dynamic.eduroam.mwn.de>
2016-12-19 13:07:19 +0100

    Ausgabe verändert

--- connectServer.c
+++ connectServer.c
@@ -15,7 +15,7 @@ int connectServer(int portnumber, char *hostname){
      * SOCK_STREAM: Typ des Sockets.(Gegensatz: SOCK_DGRAM). Stream Zuverlässiger.
      * 0: Default Transportprotokoll des Sockettyps. Von SOCK_STREAM ist dies TCP.
      */
-    printf("Connecting to %s... \n", hostname);
+    printf("Connecting to %s:%i... \n", hostname, portnumber);
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1){
         perror("Could not generate socket\n");
