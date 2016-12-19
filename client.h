f7ca30d290755826dbdd4f681c4091872b4a8eba
koellemichael <Michi@dhcp-10-180-27-101.dynamic.eduroam.mwn.de>
2016-12-19 13:07:41 +0100

    Redundante Konstanten entfernt

--- client.h
+++ client.h
@@ -18,18 +18,13 @@
 #include "parameter.h"
 #include "sharedMemory.h"
 
-//Konstanten definieren
-#define GAMEKINDNAME "Bashni"                                                   //Konstante GAMEKINDNAME definieren
-#define PORTNUMBER 1357                                                         //Konstante PORTNUMBER definieren
-#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"                               //Konstante HOSTNAME definieren
-
 //Globale Variablen deklarieren
 char *gameid;                                                                   //Globale Variable für die Game-ID deklarieren
 char *player;                                                                   //Gloabel Variable für die PlayerNr deklarieren
-char *paramNameHost = "hostName";                                               //Parametername des Hostnamens, nachdem wir filtern
-char *paramNamePort = "portNumber";                                             //Parametername der Portnummer, nach der wir filtern
-char *paramNameGame = "gameKindName";                                           //Parametername des Spiels, nach dem wir filtern
-char *value = ""; 
+char *paramNameHost = "hostname";                                               //Parametername des Hostnamens, nachdem wir filtern
+char *paramNamePort = "portnumber";                                             //Parametername der Portnummer, nach der wir filtern
+char *paramNameGame = "gamekindname";                                           //Parametername des Spiels, nach dem wir filtern
+char *value = "";
 configparams cp;                                                                //Eine Instanz vom Struct configparams namens "cp"
 char *confile;                                                                  //Speichert den Pfad der Konfigurationsfile, die geöffnet werden soll
 int pflag;                                                                      //Setzt ein Flag, wenn Player angegeben wurde
@@ -37,6 +32,5 @@ int fflag;                                                                      
 int c;                                                                          //RetValue von getopt. Entweder Argument oder -1, wenn kein Argument vorhanden
 int *shmid_player;                                                              //Gobales Int Array für die ShmIDs der Player Segmente
 struct serverinfo *serverinfo;                                                  //Gobales struct für die Serverinfos
-
 
 #endif
