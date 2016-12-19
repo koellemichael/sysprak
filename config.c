7c7c85075caac011934067186615fe13370505a4
koellemichael <Michi@dhcp-10-180-27-101.dynamic.eduroam.mwn.de>
2016-12-19 13:08:50 +0100

    createDefaultConfig hinzugefügt, Kommentare verbessert

--- config.c
+++ config.c
@@ -1,50 +1,44 @@
 #include "config.h"
 
 /**
+ *Diese Funktion liest aus der Konfigurations Datei die nötigen Parameterwerte aus und gibt sie als String zurück.
  *@param configFile Die Konfigurations Datei als Pfad
  *@param paramName als char*, der Name des Parameters
- * Diese Funktion liest aus der Konfigurations Datei die nötigen Parameterwerte aus und gibt sie als String zurück
 */
-
 char* readConfiguration(char* paramName){
-   
-   char *configFile = confile;    
+   char *configFile = confile;
    FILE *file = NULL;                                                           //Die gewählte Konfig Datei vom Typ FILE
-   char *string = malloc(256*sizeof(char));                                     //Zeilenweise Strings, die aus der Konfig Datei gelesen werden
+   char *string = malloc(BUFFERLENGTH*sizeof(char));                            //TODO speicher freigeben. Zeilenweise Strings, die aus der Konfig Datei gelesen werden
    char *delimiter = "= \n";                                                    //Tokens, die die Strings in verschiedene Abschnitte teilen
    char *substring = NULL;                                                      //Substrings, die wir rauslesen wollen
-          
-    file = fopen(configFile, "r");                                              //Öffnen der Konfigdatei
-    if(file != NULL){                                                           //Abfrage, ob sich die Datei öffnen lässt 
-        
-        while(fgets(string,256,file)) {                                         //Einzelne zeilenweise Abfrage der Datei
-            
-            if(strstr(string, paramName) != NULL){                              //Filtern nach der Zeile, die den "Hostname" enthält
-                substring = findParamValue(string, delimiter, &substring);       //Funktionsaufruf, der den zugehörigen ParameterWert zurückgibt
-                break;
-            }
-     
-        }
-    }
-    else{                                                                       //Configfile konnte nicht geöffnet werden 
-        perror("Could not open the specified config file. Please make sure the file exists in the specified directory. \n");
-    }
-    
-  fclose (file);                                                              //Schliessen der Konfig Datei
-  return substring;
 
+   file = fopen(configFile, "r");                                               //Öffnen der Konfigdatei
+   if(file != NULL){                                                            //Abfrage, ob sich die Datei öffnen lässt
+     while(fgets(string,BUFFERLENGTH,file)) {                                   //Einzelne zeilenweise Abfrage der Datei
+       if(strstr(string, paramName) != NULL){                                   //Filtern nach der Zeile, die den "Hostname" enthält
+          substring = findParamValue(string, delimiter, &substring);            //Funktionsaufruf, der den zugehörigen ParameterWert zurückgibt
+          break;
+        }
+
+     }
+   }else{                                                                       //Configfile konnte nicht geöffnet werden
+     perror("Could not open the specified config file. Please make sure the file exists in the specified directory.");
+     exit(EXIT_FAILURE);
+   }
+
+   fclose (file);                                                               //Schliessen der Konfig Datei
+   return substring;
 }
 
 /**
+ *Der Pointer zerteilt nach dem Token den String in Abschnitte und gibt den letzten Abschnitt (den Parameterwert) zurück.
  *@Param string als char* ist der zu lesende String
  *@Param delimiter als char* ist das Token, nachdem der String in Abschnitte geteilt wird
  *@Param substring als **char ist der Substring, der aus dem String gesucht wird
- *Der Pointer zerteilt nach dem Token den String in Abschnitte und gibt den letzten Abschnitt (den Parameterwert) zurück
 */
-
 char* findParamValue(char *string, char *delimiter, char **substring){
     char* ptr;                                                                  //Pointer
-    
+
     ptr = strtok(string, delimiter);                                            //Zeigt auf das erste Token
         while(ptr != NULL){                                                     //Während der Pointer noch nicht am Ende ist
             *substring = ptr;                                                   //Der Substring ist jeweils der nächste Abschnitt
@@ -52,4 +46,25 @@ char* findParamValue(char *string, char *delimiter, char **substring){
             ptr = strtok(NULL, delimiter);                                      //Der Pointer geht eins weiter
         }
     return *substring;
+}
+
+/**
+ *Erstellt eine Default Config wenn noch keine existiert.
+ */
+void createDefaultConfig(void){
+  FILE *file = NULL;
+  file = fopen(confile, "r");                                                   //Config versuchen zu öffnen NULL falls nicht existent
+  if(file == NULL) {
+    perror("Can not find config file, creating default config");                //Falls nicht existent, erstelle default config
+    file = fopen(confile, "w");                                                 //Erstele file und öffne im Schreibmodus
+    if(file == NULL) {                                                          //Überprüfen ob auf die Datei zugegriffen werden kann
+  	   perror("No permission to create default config");
+    }else {
+      fprintf(file, "hostname=%s\nportnumber=%i\ngamekindname=%s",              //Inhalt der Default Config reinschreiben
+      HOSTNAME,PORTNUMBER,GAMEKINDNAME);
+      fclose(file);                                                             //File schließen
+    }
+  }else{
+    fclose(file);                                                               //File schließen
+  }
 }
