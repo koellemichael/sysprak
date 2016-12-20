6b8c1b8eae3dd44d60e85ca6c7732497a8a2029b
koellemichael <Michi@Michaels-MBP.fritz.box>
2016-12-20 01:41:34 +0100

    processGame.h hinzugefügt

---
+++ processGame.h
@@ -0,0 +1,16 @@
+#ifndef processGam                                                              //Wenn das Makro noch nicht existiert
+#define processGam                                                              //Definiere das Makro
+
+//Bibliotheken einbinden
+
+//Module einbinden
+#include "parameter.h"
+
+//Funktionen deklarieren
+void wait(void);
+void gameover(void);
+void move(void);
+void moveok(void);
+void processGame(void);
+
+#endif
