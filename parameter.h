eef42808d0389996ebbf440a0593d44c4bab54a3
koellemichael <Michi@Michaels-MBP.fritz.box>
2016-12-23 02:36:16 +0100

    Konstante für die Reihen und Spaltenanzahl des Spielfelds hinzugefügt

--- parameter.h
+++ parameter.h
@@ -10,6 +10,8 @@
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"                               //Konstante HOSTNAME definieren
 #define BUFFERLENGTH 1024                                                       //Konstante für die Puffergröße definieren
 #define VERSION "2.3"                                                           //TODO provisorische Konstante
+#define ROWS 8                                                                  //Konstante für die Reihen des Spielfelds
+#define COLUMNS 8                                                               //Konstante für die Spalten des Spielfelds
 
 //Funktionen definieren
 void printLogo(void);
