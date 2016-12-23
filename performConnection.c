80168ff1bd962a522b87c4d0c57d0fdf5611baf1
koellemichael <Michi@Michaels-MBP.fritz.box>
2016-12-23 02:36:28 +0100

    Ausgabe entfernt

--- performConnection.c
+++ performConnection.c
@@ -20,7 +20,6 @@ void performConnection(int sock){
        end = !match(requests[x]+2,"QUIT");                                      //Wurde +ENDPLAYERS gesendet?
         if(buffer[0]=='+'){                                                     //Wenn Serveranfrage positiv ausfällt
           if(strlen(requests[x])>2){                                            //Leere Anfrage vom Server ignorieren               //TODO Fehlerbehandlung
-            printf("server RAW: %s\n",requests[x]);
             char *response = handle(requests[x]+2);                             //Sucht die passende Anfrage auf die Serveranfrage
             if (response!=NULL){                                                //Wenn es eine Anfrage gibt
                 send(sock,response,strlen(response),0);                         //Sendet dem Server die Antwort des Clients
