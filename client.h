65604282c1e861fa3b3be639b3db039720a62bda
KathiWinter <kathi.winter@t-online.de>
2016-12-26 10:32:48 +0100

    Pipe Grundgerüst eingerichtet und think Datei erstellt

--- client.h
+++ client.h
@@ -11,6 +11,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include "signal.h"
+#include "think.h"
 
 //Module einbinden
 #include "performConnection.h"
---
+++ think.h
@@ -0,0 +1,16 @@
+#ifndef thinking
+#define thinking
+
+//Bibliotheken einbinden
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//Variablen einbinden
+char* string;
+
+//Funktionen deklarieren
+int think(int* fd);
+
+#endif
