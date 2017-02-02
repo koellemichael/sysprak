6af63b84ef7c3d0a970efb194c439236c599e94d
koellemichael <Michi@Michaels-MBP.fritz.box>
2017-02-02 11:25:30 +0100

    if update

--- think.c
+++ think.c
@@ -112,7 +112,7 @@ movearray calcPossibleMoves(int i, int j){
   if(isQueen(i,j)==1){
     for(int a=-ROWS; a<ROWS; a++){
       for (int b=-COLUMNS; b<COLUMNS; b++){
-        if(!(a==0&&b==0) && abs(a)==abs(b) && (i+a)>0 && (j+b)>0 && (i+a)<ROWS && (j+b)<COLUMNS){
+        if(!(a==0&&b==0) && abs(a)==abs(b) && (i+a)>0 && (j+b)>0 && (i+a)<=ROWS && (j+b)<=COLUMNS){
           printf("i+a: %i j+b: %i \n",i+a,j+b);
             switch (isAlly(i+a,j+b)){
                 case 0:  vza= a+ (int)(abs(a)/a);                                            //Vorzeichen: wenn a negativ, dann -1 addiert
@@ -149,7 +149,8 @@ movearray calcPossibleMoves(int i, int j){
   }else{
     for(int x = -1; x<2;x++){
       for(int y = -1; y<2;y++){
-        if(!(x==0&&y==0) && abs(x)==abs(y) && (i+x)>0 && (j+y)>0 && (i+x)<ROWS && (j+y)<COLUMNS){
+        if(!(x==0&&y==0) && abs(x)==abs(y) && (i+x)>0 && (j+y)>0 && (i+x)<=ROWS && (j+y)<=COLUMNS){
+          printf("%i %i\n",i+x,j+y );
           memset(possibleMoves.moves[p].move,0,strlen(possibleMoves.moves[p].move));
           possibleMoves.moves[p].weight = 0;
           switch (isAlly(i+x,j+y)){
@@ -184,7 +185,7 @@ void jump (int i, int j, movearray *possibleMoves, int p){
   if(isQueen(i, j)==1){
        for(int a = -8; a<8;a++){
           for(int b = -8; b<8; b++){
-              if(!(a==0&&b==0) && abs(a)==abs(b) && (i+a)>0 && (j+b)>0 && (i+a)<ROWS && (j+b)<COLUMNS){
+            if(!(a==0&&b==0) && abs(a)==abs(b) && (i+a)>0 && (j+b)>0 && (i+a)<=ROWS && (j+b)<=COLUMNS){
               char *onemore = malloc(sizeof(char)*BUFFERLENGTH_MOVE);
               memset(onemore, 0, strlen(onemore));
               sprintf(onemore, ":%c%i",inttocolumn(j+(vzcol)*vza),COLUMNS-(i+(vzcol)*vzb));
@@ -198,15 +199,14 @@ void jump (int i, int j, movearray *possibleMoves, int p){
                   printf("jump2\n");
                   jump(j+(vzcol)*vza,COLUMNS-(i+(vzcol)*vzb), possibleMoves, p);
               }
-         }
-      }
-  }
-
+            }
+          }
+        }
   }else{
     printf("jump1\n");
     for(int x = -1; x<2;x++){
       for(int y = -1; y<2;y++){
-        if(!(a==0&&b==0) && abs(a)==abs(b) && (i+a)>0 && (j+b)>0 && (i+a)<ROWS && (j+b)<COLUMNS){
+        if(!(x==0&&y==0) && abs(x)==abs(y) && (i+x)>0 && (j+y)>0 && (i+x)<=ROWS && (j+y)<=COLUMNS){
           char *onemore = malloc(sizeof(char)*BUFFERLENGTH_MOVE);
           memset(onemore, 0, strlen(onemore));
           sprintf(onemore, ":%c%i",inttocolumn(j+(2*y)),COLUMNS-(i+(2*x)));
